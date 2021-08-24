b66855e5a4dc99a97e94ef37a1d14b0fbe1a845b
zeniakiran <67507784+zeniakiran@users.noreply.github.com>
2021-08-24 09:55:24 +0500

    test api

---
+++ db/models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  /* const User = sequelize.define("users", {
+    title: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    },
+    published: {
+      type: Sequelize.BOOLEAN
+    }
+  }); */
+  return User;
+};
